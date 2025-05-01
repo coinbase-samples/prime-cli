5e67f2896b6f3ba7ee49ea299fd095adbb89b629
Ryan Nitz <ryan.nitz@coinbase.com>
2025-05-01 16:55:58 -0400

    Dev

---
+++ cmd/balances/list_portfolio.go
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2023-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package balances
+
+import (
+	"fmt"
+
+	"github.com/coinbase-samples/prime-cli/utils"
+	"github.com/coinbase-samples/prime-sdk-go/balances"
+
+	"github.com/spf13/cobra"
+)
+
+var listPortfolioBalancesCmd = &cobra.Command{
+	Use:   "list-portfolio",
+	Short: "Lists portfolio balances that meet filter criteria",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		client, err := utils.GetClientFromEnv()
+		if err != nil {
+			return fmt.Errorf("failed to initialize client: %w", err)
+		}
+
+		balancesService := balances.NewBalancesService(client)
+
+		portfolioId, err := utils.GetPortfolioId(cmd, client)
+		if err != nil {
+			return err
+		}
+
+		symbols, err := cmd.Flags().GetStringSlice(utils.SymbolsFlag)
+		if err != nil {
+			return fmt.Errorf("cannot get symbols: %w", err)
+		}
+
+		ctx, cancel := utils.GetContextWithTimeout()
+		defer cancel()
+
+		request := &balances.ListPortfolioBalancesRequest{
+			PortfolioId: portfolioId,
+			Type:        utils.GetFlagStringValue(cmd, utils.TypeFlag),
+			Symbols:     symbols,
+		}
+
+		response, err := balancesService.ListPortfolioBalances(ctx, request)
+		if err != nil {
+			return fmt.Errorf("cannot list portfolio balances: %w", err)
+		}
+
+		jsonResponse, err := utils.FormatResponseAsJson(cmd, response)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(jsonResponse)
+
+		return nil
+	},
+}
+
+func init() {
+	Cmd.AddCommand(listPortfolioBalancesCmd)
+
+	listPortfolioBalancesCmd.Flags().StringArrayP(utils.TypeFlag, "t", []string{}, "Type of balance")
+	listPortfolioBalancesCmd.Flags().StringSliceP(utils.SymbolsFlag, "s", []string{}, "List of symbols")
+	listPortfolioBalancesCmd.Flags().StringP(utils.PortfolioIdFlag, "", "", "Portfolio ID. Uses environment variable if blank")
+}
