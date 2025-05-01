aeac1cce79f413a3453d4700fa743ac87e4de92a
Ryan Nitz <ryan.nitz@coinbase.com>
2025-05-01 16:02:00 -0400

    Dev

---
+++ cmd/portfolios/get.go
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2023-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package portfolios
+
+import (
+	"fmt"
+
+	"github.com/coinbase-samples/prime-cli/utils"
+	"github.com/coinbase-samples/prime-sdk-go/portfolios"
+	"github.com/spf13/cobra"
+)
+
+var getPortfolioCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Retrieve portfolio information using portfolio ID.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := utils.GetClientFromEnv()
+		if err != nil {
+			return fmt.Errorf("cannot get client from environment: %w", err)
+		}
+
+		portfoliosService := portfolios.NewPortfoliosService(client)
+
+		portfolioId, err := utils.GetPortfolioId(cmd, client)
+		if err != nil {
+			return err
+		}
+
+		ctx, cancel := utils.GetContextWithTimeout()
+		defer cancel()
+
+		request := &portfolios.GetPortfolioRequest{
+			PortfolioId: portfolioId,
+		}
+		response, err := portfoliosService.GetPortfolio(ctx, request)
+		if err != nil {
+			return fmt.Errorf("cannot get portfolio: %w", err)
+		}
+
+		jsonResponse, err := utils.FormatResponseAsJson(cmd, response)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(jsonResponse)
+		return nil
+	},
+}
+
+func init() {
+	PortfoliosCmd.AddCommand(getPortfolioCmd)
+
+	getPortfolioCmd.Flags().StringP(utils.PortfolioIdFlag, "", "", "Portfolio ID. Uses environment variable if blank")
+
+}
