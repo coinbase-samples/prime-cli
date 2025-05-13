34d8690b45832f8b0a14cc10f215796120c8b6da
Ryan Nitz <ryan.nitz@coinbase.com>
2025-05-13 11:34:05 -0400

    Add pagination support

--- cmd/orders/list_open.go
+++ cmd/orders/list_open.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 
 	"github.com/coinbase-samples/prime-cli/utils"
+	"github.com/coinbase-samples/prime-sdk-go/model"
 	"github.com/coinbase-samples/prime-sdk-go/orders"
 
 	"github.com/spf13/cobra"
@@ -34,41 +35,100 @@ var listOpenOrdersCmd = &cobra.Command{
 			return fmt.Errorf("failed to initialize client: %w", err)
 		}
 
-		ordersService := orders.NewOrdersService(client)
+		svc := orders.NewOrdersService(client)
 
 		portfolioId, err := utils.GetPortfolioId(cmd, client)
 		if err != nil {
 			return err
 		}
 
-		ctx, cancel := utils.GetContextWithTimeout()
-		defer cancel()
-
-		request := &orders.ListOpenOrdersRequest{
-			PortfolioId: portfolioId,
-			ProductId:   utils.GetFlagStringValue(cmd, utils.ProductIdFlag),
-		}
-
-		response, err := ordersService.ListOpenOrders(ctx, request)
-		if err != nil {
-			return fmt.Errorf("cannot list open orders: %w", err)
-		}
-
-		jsonResponse, err := utils.FormatResponseAsJson(cmd, response)
+		productIds, err := cmd.Flags().GetStringSlice(utils.ProductIdsFlag)
 		if err != nil {
 			return err
 		}
 
-		fmt.Println(jsonResponse)
-		return nil
+		orderType, err := cmd.Flags().GetString(utils.OrderTypeFlag)
+		if err != nil {
+			return err
+		}
+
+		orderSide, err := cmd.Flags().GetString(utils.OrderSideFlag)
+		if err != nil {
+			return err
+		}
+
+		startStr, err := cmd.Flags().GetString(utils.StartFlag)
+		if err != nil {
+			return err
+		}
+
+		endStr, err := cmd.Flags().GetString(utils.EndFlag)
+		if err != nil {
+			return err
+		}
+
+		start, end, err := utils.ParseDateRange(startStr, endStr)
+		if err != nil {
+			return err
+		}
+
+		request := &orders.ListOpenOrdersRequest{
+			PortfolioId: portfolioId,
+			ProductIds:  productIds,
+			OrderType:   orderType,
+			OrderSide:   orderSide,
+			Start:       start,
+			End:         end,
+		}
+
+		return utils.HandleListCmd(
+			cmd,
+			func(paginationParams *model.PaginationParams) (*model.Pagination, error) {
+
+				request.Pagination = paginationParams
+
+				response, err := listOpenOrders(svc, request)
+				if err != nil {
+					return nil, err
+				}
+
+				if err := utils.PrintJsonDocs(cmd, response.Orders); err != nil {
+					return nil, err
+				}
+
+				return response.Pagination, nil
+			},
+		)
 	},
+}
+
+func listOpenOrders(
+	svc orders.OrdersService,
+	req *orders.ListOpenOrdersRequest,
+) (*orders.ListOpenOrdersResponse, error) {
+
+	ctx, cancel := utils.GetContextWithTimeout()
+	defer cancel()
+
+	response, err := svc.ListOpenOrders(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("cannot list open orders: %w", err)
+	}
+
+	return response, nil
 }
 
 func init() {
 	Cmd.AddCommand(listOpenOrdersCmd)
 
-	listOpenOrdersCmd.Flags().String(utils.ProductIdFlag, "", "ID of the product")
 	utils.AddPortfolioIdFlag(listOpenOrdersCmd)
+	utils.AddProductIdsFlag(listOpenOrdersCmd)
+	utils.AddPaginationFlags(listOpenOrdersCmd, false)
+	utils.AddSortDirectionFlag(listOpenOrdersCmd)
+	utils.AddStartEndFlags(listOpenOrdersCmd)
 
-	listOpenOrdersCmd.MarkFlagRequired(utils.ProductIdFlag)
+	utils.AddOrderSideFlag(listOpenOrdersCmd)
+	utils.AddOrderTypeFlag(listOpenOrdersCmd)
+
+	// listOpenOrdersCmd.MarkFlagRequired(utils.ProductIdsFlag)
 }
