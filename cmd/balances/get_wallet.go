ff23c5feb9bd333cd5185c37b903ee5a1a8049e5
Ryan Nitz <ryan.nitz@coinbase.com>
2026-06-17 14:59:10 -0400

    Package

--- cmd/balances/get_wallet.go
+++ cmd/balances/get_wallet.go
@@ -20,7 +20,7 @@ import (
 	"fmt"
 
 	"github.com/coinbase-samples/prime-cli/utils"
-	"github.com/coinbase-samples/prime-sdk-go/balances"
+	"github.com/coinbase/prime-sdk-go/balances"
 
 	"github.com/spf13/cobra"
 )
--- cmd/balances/list_entity.go
+++ cmd/balances/list_entity.go
@@ -20,8 +20,8 @@ import (
 	"fmt"
 
 	"github.com/coinbase-samples/prime-cli/utils"
-	"github.com/coinbase-samples/prime-sdk-go/balances"
-	"github.com/coinbase-samples/prime-sdk-go/model"
+	"github.com/coinbase/prime-sdk-go/balances"
+	"github.com/coinbase/prime-sdk-go/model"
 
 	"github.com/spf13/cobra"
 )
