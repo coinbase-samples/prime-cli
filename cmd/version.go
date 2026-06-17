4286ce7898df6c8cb39bd32d9f0115c03567f0e6
Ryan Nitz <ryan.nitz@coinbase.com>
2026-06-17 15:01:24 -0400

    Increase

--- cmd/version.go
+++ cmd/version.go
@@ -25,7 +25,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var primectlVersion = `{"version":"0.4.1"}`
+var primectlVersion = `{"version":"0.4.2"}`
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
